c61b3be87f23ca11358b12bbfbf3455e4103a972
XuTTcTeP <zxcvb.ru@inbox.ru>
2018-06-06 10:43:42 +0300

    @Mod annotation

---
+++ src/main/java/com/hipter57/bitofeverything/BitOfEverything.java
@@ -0,0 +1,11 @@
+package com.hipter57.bitofeverything;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(modid = Reference.MODID , name = Reference.NAME , version = Reference.VERSION)
+public class BitOfEverything {
+
+    
+
+}
---
+++ src/main/java/com/hipter57/bitofeverything/Reference.java
@@ -0,0 +1,9 @@
+package com.hipter57.bitofeverything;
+
+public class Reference {
+
+    public static final String MODID = "boe";
+    public static final String NAME = "Bit of Everything";
+    public static final String VERSION = "0.1.0";
+
+}
